62e54809856adbede719ed0065f91e5bc9930278
imekachi <saran@dek-d.com>
2017-11-19 23:29:12 +0700

    - removed mock player image

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import imgPlayerMock from '../images/player-mock.png'
 import imgMascot from '../images/mascot.png'
 
 const Avatar = styled.div`
@@ -7,7 +6,7 @@ const Avatar = styled.div`
   vertical-align: middle;
   border: 4px solid #FFFFFF;
   border-radius: 50%;
-  background: url(${props => (props.mascot) ? imgMascot : props.img || imgPlayerMock}) #CCCCCC no-repeat center center;
+  background: url(${props => (props.mascot) ? imgMascot : props.img}) #CCCCCC no-repeat center center;
   background-size: cover;
   box-shadow: inset 0 0 8px -1px rgba(0,0,0,0.3);
   width: ${props => props.size || '150px'};
