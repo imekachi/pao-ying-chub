45b76f2d928ba331f50058d3025e2aef21f1edfb
imekachi <saran@dek-d.com>
2017-11-20 22:04:55 +0700

    - adjust playerInfo style and sizes

--- src/components/PlayersInfo.js
+++ src/components/PlayersInfo.js
@@ -5,6 +5,7 @@ import imgAvatarAI from '../images/avatar-ai.png'
 import imgAvatarPlayerFrame from '../images/avatar-player.png'
 import imgBGVersus from '../images/scene-3.jpg'
 import { getUserProfile } from '../util/facebook'
+import { parsePixel } from '../util/unitConverter'
 import MainLayout from './MainLayout'
 
 const Container = styled.div`
@@ -12,7 +13,7 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  margin: 12% auto;
+  margin: 10% auto 7%;
   position: fixed;
   left: 50%;
   top: 0;
@@ -20,25 +21,28 @@ const Container = styled.div`
   transform: translateX(-50%);
 `
 
+const avatarSize = 130
+
 const AvatarAI = styled.img`
   display: block;
-  width: 140px;
+  width: ${parsePixel(avatarSize)};
 `
 
 const PlayerFrame = styled.div`
   position: relative;
-  width: 140px;
-  height: 188px;
+  width: ${parsePixel(avatarSize)};
+  height: ${parsePixel(avatarSize * 1.34285714)};
   background: url(${imgAvatarPlayerFrame}) no-repeat center center;
   background-size: contain;
 `
+const plyerImgSize = 101 // avatarSize * 0.785714286
 
 const PlayerImg = styled.div`
   position: absolute;
   top: 11px;
   left: 15px;
-  width: 110px;
-  height: 110px;
+  width: ${parsePixel(plyerImgSize)};
+  height: ${parsePixel(plyerImgSize)};
   border-radius: 15px;
   background: url(${props => props.src}) no-repeat center center;
   background-size: cover;
